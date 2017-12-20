5e2284128a2b3db08602c7ac93a3a63aa9a9d777
Kuan Hou Lee <angus.lee809@gmail.com>
2017-12-21 02:21:09 +1100

    Button will log to console

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("test-button").addEventListener("click", getPrice)
+})
+
+function getPrice()
+{
+    console.log("Hello world!");
+}

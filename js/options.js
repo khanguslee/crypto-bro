fcffa69ae046b4e396dfe37d4f5193c3097e4378
khanguslee <angus.lee809@gmail.com>
2017-12-25 21:53:09 +1100

    Saved options to chrome storage TODO: Checkbox set if it is already selected from previous session

--- js/options.js
+++ js/options.js
@@ -1,23 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    getAllCoins();
+    displayAllCoins();
+    syncList();
 });
 
 const cmcBaseUrl = 'https://api.coinmarketcap.com/v1';
 
-function getAllCoins() {
+function syncList() {
+    chrome.storage.sync.get('coins', (result) => {
+        console.log(result);
+    })
+}
+
+function displayAllCoins() {
     fetch(cmcBaseUrl + '/ticker/?limit=0')
     .then((response) => response.json())
     .then((data) => {
         let coinList = document.getElementById('coin-option-list')
         for (let i = 0; i< data.length; i++)
         {
-            console.log(data[i]);
+            //console.log(data[i]);
             let newCoinEntry = document.createElement('li');
-            newCoinEntry.id = "coin-entry";
+            newCoinEntry.id = 'coin-entry';
 
             let checkbox = document.createElement('input');
-            checkbox.type = "checkbox";
-            checkbox.id = "cb-" + data[i].name;
+            checkbox.type = 'checkbox';
+            checkbox.id = 'cb-' + data[i].id;
             checkbox.addEventListener('click', updateList);
             newCoinEntry.appendChild(checkbox);
 
@@ -28,5 +35,16 @@ function getAllCoins() {
 }
 
 function updateList() {
-    console.log(this.id);
+    let coinName = this.id.substr(3);
+
+    var coinList;
+    chrome.storage.sync.get('coins', (result) => {
+        coinList = result["coins"];
+        coinList[coinName] = this.checked ? true : false;
+        chrome.storage.sync.set({
+            'coins' : coinList
+        }, () => {
+            console.log('Settings saved');
+        });
+    }); 
 }

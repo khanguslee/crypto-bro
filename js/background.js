65a343be772356a592383673a9a41dd39fc9df90
Kuan Hou Lee <angus.lee809@gmail.com>
2018-01-17 02:52:21 +1100

    Alerts now work. Will need to notify user now

--- js/background.js
+++ js/background.js
@@ -1,10 +1,11 @@
+const cmcBaseUrl = 'https://api.coinmarketcap.com/v1';
+
 function updateCoinList() {
     // Get currency to display
     chrome.storage.sync.get({"currency": "USD"}, (storedCurrency) => {
         var currency = storedCurrency.currency;
         console.log('Updating coin data with ' + currency);
 
-        const cmcBaseUrl = 'https://api.coinmarketcap.com/v1';
         // Set URL to get information from
         var coinURL = cmcBaseUrl + '/ticker/?limit=0';
         if (currency != "USD") {
@@ -24,6 +25,51 @@ function updateCoinList() {
     });
 }
 
+function checkAlerts() {
+    /*
+        Will check if user needs to be alerted about the price
+    */
+    // Get stored alert options for each coin
+    const defaultJsonValue = {'coinOptions':{'bitcoin': {"alert": {}}}};
+    chrome.storage.sync.get(defaultJsonValue, (result) => {
+        var coinList = result.coinOptions;
+        // Get coin data stored locally 
+        chrome.storage.local.get({'coins': {}}, (result) => {
+            let coinValueList = result.coins;
+            for(let coin in coinValueList) {
+                // If the user has set some options for the coin in the list
+                if (coinValueList[coin].id in coinList)
+                {
+                    let coinOptions = coinList[coinValueList[coin].id];
+                    // Check if we have set some alert options
+                    if (!('alert' in coinOptions)) {
+                        continue;
+                    } 
+                    // Check if alert is empty
+                    if (coinOptions.alert == "" || coinOptions.alert == {}) {
+                        continue;
+                    }
+                    let coinCurrency = coinOptions.alert.currency;  // Either USD or BTC
+                    let coinMinAmount = coinOptions.alert.minAmount;
+                    let coinMaxAmount = coinOptions.alert.maxAmount;
+                    let priceString = 'price_' + coinCurrency.toLowerCase();
+                    let currentCoinPrice = coinValueList[coin][priceString];
+
+                    if (currentCoinPrice < coinMinAmount) {
+                        // Notify user
+                    }
+
+                    if (currentCoinPrice > coinMaxAmount) {
+                        // Notify user
+                    }
+
+                }
+            }
+        });
+    });
+}
+
 setInterval(() => {
     updateCoinList();
-}, 10000);
+    checkAlerts();
+}, 3000);
